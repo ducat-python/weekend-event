50a57f731a0fb493300cbbb5dcb4557f24f97ee9
Piyush <piyush.maurya@outlook.in>
2017-07-15 13:41:39 +0530

    email message added

--- attendees/views.py
+++ attendees/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
 
 from .models import Attendee
 from shows.models import Show
@@ -20,6 +21,9 @@ def register_attendee(request, pk=None):
         if form.is_valid():
             instance = form.save(commit=False)
             instance.show = show
+            subject = 'Congrats! you are successfully registered for ' + show.title
+            message = 'Hi' + instance.name + '\nYou have successfully registered for ' + show.title + '\n \nThank you for using our application. \nDo come again.\nSupport: care@myapp.com'
+            send_mail(subject, message, 'you_gmail_id', [instance.email])
             instance.save()
             return HttpResponseRedirect('/')
     else:
