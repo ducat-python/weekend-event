9624668c5d241195730dd7200ed60b565347a7d8
Piyush <piyush.maurya@outlook.in>
2017-07-15 13:30:56 +0530

    email settings added

--- events/settings.py
+++ events/settings.py
@@ -101,6 +101,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True   
+
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
